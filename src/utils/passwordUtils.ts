e8871ddde353bb5d7d9cb1fbd4ce91781cf7ae75
quantum-builds <muhammadmassab.dev@gmail.com>
2025-01-16 00:18:46 +0500

    fix: npm build fix

--- src/utils/passwordUtils.ts
+++ src/utils/passwordUtils.ts
@@ -1,11 +1,13 @@
-import bcrypt from 'bcrypt';
-
+import bcrypt from "bcryptjs";
 
 export const hashPassword = async (password: string): Promise<string> => {
   const saltRounds = 10; // You can adjust the salt rounds as needed
   return await bcrypt.hash(password, saltRounds);
 };
 
-export const verifyPassword = async (plainPassword: string, hashedPassword: string): Promise<boolean> => {
+export const verifyPassword = async (
+  plainPassword: string,
+  hashedPassword: string
+): Promise<boolean> => {
   return await bcrypt.compare(plainPassword, hashedPassword);
 };
