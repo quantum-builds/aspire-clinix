66551c0d58218287f4ea843b5d5be2acc8d0bfec
quantum-builds <muhammadmassab.dev@gmail.com>
2025-02-11 16:34:12 +0500

    add asthetics and wellness pages

---
+++ src/app/(landing)/asthetics/page.tsx
@@ -0,0 +1,141 @@
+import AboutUsSection from "../../../components/AboutUsSection";
+
+import HeroSection from "../../../components/HeroSection";
+import DentistryDetailSection from "../../../components/DentistryDetailSection";
+import {
+  AiCartoon,
+  ClinicChair,
+  DentistTreatment,
+  image1,
+  image2,
+  image3,
+  TeethCartoon,
+} from "@/assets";
+
+const HERO_SECTION_DATA = {
+  title: "ASTHETICS",
+  titleTextSize: 64,
+  titleLineHeight: 72.64,
+  description:
+    "Lorem ipsum odor amet, consectetuer adipiscing elit. Sociosqu a nec magna habitant nec. Ullamcorper dui varius volutpat primis lacinia elit morbi velit.",
+  descTextSize: 32,
+  descLineHeight: 36.36,
+  contentWidth: 100,
+  backgroundColor: "#D9D9D9",
+  buttonColor: "#EBEBEB",
+};
+
+const ABOUT_US_SECTION_DATA = {
+  description:
+    "Lorem ipsum odor amet, consectetuer adipiscing elit. Sociosqu a nec magna habitant nec. Ullamcorper dui varius volutpat primis lacinia elit morbi velit.",
+  descriptionTextSize: 40,
+  descriptionLeadingHeight: 45.4,
+  hasButton: false,
+  buttonText: null,
+  backgroundColor: "#ECE8E3",
+  textColor: "#382F26",
+  descriptionWidth: 60,
+};
+
+const ASTHETICS_SERVICES = {
+  services: [
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image1,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image2,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image3,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image1,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image2,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image3,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image1,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image2,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image3,
+    },
+    {
+      title: "General Dentistry",
+      description:
+        "Ullamcorper dui varius volutpat primis lacinia elit morbi velit. Lorem ipsum odor amet, consectetuer adipiscing elit. ",
+      path: "/",
+      backgroundContent: image1,
+    },
+  ],
+};
+
+export default function Asthetics() {
+  return (
+    <div className="flex flex-col" id="asthetics">
+      <HeroSection
+        title={HERO_SECTION_DATA.title}
+        description={HERO_SECTION_DATA.description}
+        contentWidth={HERO_SECTION_DATA.contentWidth}
+        backgroundColor={HERO_SECTION_DATA.backgroundColor}
+        buttonColor={HERO_SECTION_DATA.buttonColor}
+        descLineHeight={36.6}
+        titleFontSize="md:text-[45px] text-[25px] lg:text-[64px]"
+        isVideo={false}
+        backgroundContent={ClinicChair}
+      />
+      <AboutUsSection
+        description={ABOUT_US_SECTION_DATA.description}
+        buttonBackgroundColor="#ECE8E3"
+        hasButton={ABOUT_US_SECTION_DATA.hasButton}
+        buttonText={ABOUT_US_SECTION_DATA.buttonText}
+        backgroundColor={ABOUT_US_SECTION_DATA.backgroundColor}
+        textColor={ABOUT_US_SECTION_DATA.textColor}
+        buttonClickLink={"/"}
+      />
+      <DentistryDetailSection services={ASTHETICS_SERVICES.services} />
+    </div>
+  );
+}
