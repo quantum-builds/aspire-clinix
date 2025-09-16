bed627e211dafe2791c30238ff09d23443217cf2
quantum-builds <muhammadmassab.dev@gmail.com>
2025-09-16 20:17:57 +0500

    update:pixels

--- src/app/(landing)/privacy-policy/page.tsx
+++ src/app/(landing)/privacy-policy/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 import { AspireDarkLogo } from "@/assets";
 import HeroNav from "@/components/HeroNav";
 
@@ -13,17 +12,12 @@ const PrivacyPolicy: React.FC = () => {
         buttonColor={"#D9D9D9"}
       />
 
-      <Head>
-        <title>Privacy Policy</title>
-        <meta name="description" content="Privacy Policy" />
-      </Head>
-
       <main className="w-11/12 md:w-4/5 mx-auto py-16 md:py-24 lg:py-36">
-        <h1 className="text-5xl font-medium text-trueBlack mt-10 mb-5 font-opus">
+        <h1 className="text-4xl font-medium text-trueBlack mt-10 mb-5 font-opus">
           Privacy Policy
         </h1>
 
-        <p className="text-trueBlack font-gillSans mb-6 text-2xl">
+        <p className="text-trueBlack font-gillSans mb-6 text-xl">
           Lorem ipsum dolor amet, consectetur adipiscing elit. Socioqua a nec
           magna habitant nec. Ullamcorper dui verius volutpat primis lacinia
           elit morbi velit. Ullamcorper dui verius volutpat primis lacinia elit
@@ -31,11 +25,11 @@ const PrivacyPolicy: React.FC = () => {
           ipsum dolor amet, consectetur adipiscing elit.
         </p>
 
-        <h2 className="text-4xl font-medium text-trueBlack font-gillSans mt-10 mb-5 ">
+        <h2 className="text-3xl font-medium text-trueBlack font-gillSans mt-10 mb-5 ">
           Subheading
         </h2>
 
-        <p className="text-trueBlack font-gillSans mb-6 text-2xl">
+        <p className="text-trueBlack font-gillSans mb-6 text-xl">
           Lorem ipsum dolor amet, consectetur adipiscing elit. Socioqua a nec
           magna habitant nec. Ullamcorper dui verius volutpat primis lacinia
           elit morbi velit. Ullamcorper dui verius volutpat primis lacinia elit
@@ -43,7 +37,7 @@ const PrivacyPolicy: React.FC = () => {
           ipsum dolor amet, consectetur adipiscing elit.
         </p>
 
-        <p className="text-trueBlack font-gillSans mb-6 text-2xl">
+        <p className="text-trueBlack font-gillSans mb-6 text-xl">
           Lorem ipsum dolor amet, consectetur adipiscing elit. Socioqua a nec
           magna habitant nec. Ullamcorper dui verius volutpat primis lacinia
           elit morbi velit. Ullamcorper dui verius volutpat primis lacinia elit
@@ -51,11 +45,11 @@ const PrivacyPolicy: React.FC = () => {
           ipsum dolor amet, consectetur adipiscing elit.
         </p>
 
-        <h2 className="text-4xl font-medium text-trueBlack font-opus mt-10 mb-5">
+        <h2 className="text-3xl font-medium text-trueBlack font-opus mt-10 mb-5">
           Subheading
         </h2>
 
-        <p className="text-trueBlack font-gillSans mb-6 text-2xl">
+        <p className="text-trueBlack font-gillSans mb-6 text-xl">
           Lorem ipsum dolor amet, consectetur adipiscing elit. Socioqua a nec
           magna habitant nec. Ullamcorper dui verius volutpat primis lacinia
           elit morbi velit. Ullamcorper dui verius volutpat primis lacinia elit
@@ -63,7 +57,7 @@ const PrivacyPolicy: React.FC = () => {
           ipsum dolor amet, consectetur adipiscing elit.
         </p>
 
-        <p className="text-trueBlack font-gillSans mb-6 text-2xl">
+        <p className="text-trueBlack font-gillSans mb-6 text-xl">
           Lorem ipsum dolor amet, consectetur adipiscing elit. Socioqua a nec
           magna habitant nec. Ullamcorper dui verius volutpat primis lacinia
           elit morbi velit. Ullamcorper dui verius volutpat primis lacinia elit
@@ -71,7 +65,7 @@ const PrivacyPolicy: React.FC = () => {
           ipsum dolor amet, consectetur adipiscing elit.
         </p>
 
-        <ul className="list-disc pl-6 mb-6 text-trueBlack font-gillSans text-2xl">
+        <ul className="list-disc pl-6 mb-6 text-trueBlack font-gillSans text-xl">
           <li className="mb-2">
             Lorem ipsum dolor amet, consectetur adipiscing elit.
           </li>
