e2ebef3d0f59ea9315f9d27a72fd81621360613d
quantum-builds <muhammadmassab.dev@gmail.com>
2025-10-01 03:13:33 +0500

    fix: button-text

--- src/app/(dashboards)/patient/(public)/register/page.tsx
+++ src/app/(dashboards)/patient/(public)/register/page.tsx
@@ -9,7 +9,7 @@ export default function RegisterPage() {
       <BackButton
         className="bg-gray hover:bg-lightGray w-fit mb-5"
         backToWebsite={true}
-        text="Back To Home"
+        text="Back To Website"
       />
 
       <div className="mx-auto w-full flex flex-col bg-dashboardBarBackground rounded-2xl  justify-center max-w-lg p-6 md:p-10">
