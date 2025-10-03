90bb17a084b8120163e4ba1d5d0eb99b311f50a1
quantum-builds <muhammadmassab.dev@gmail.com>
2025-10-03 15:21:41 +0500

    fix: fixed practices pages height

--- src/app/(dashboards)/clinic/(protected)/practice/[id]/page.tsx
+++ src/app/(dashboards)/clinic/(protected)/practice/[id]/page.tsx
@@ -1,24 +1,17 @@
 import PageTopBar from "@/app/(dashboards)/components/custom-components/PageTopBar";
 import CustomButton from "@/app/(dashboards)/components/custom-components/CustomButton";
-import { TPractice } from "@/types/practice";
-import PracticeDetails from "./components/PracticeDetails";
-import { PracticeDentistDataTable } from "./components/PracticeDentistsDataTable";
-import {
-  DentistRole,
-  GenderType,
-  PracticeApprovalStatus,
-} from "@prisma/client";
 import { Suspense } from "react";
 import PracticeDentistsDataTableWrapper from "./components/PracticeDentistsDataTableWrapper";
 import PracticeDetailsSkeleton from "./components/skeletons/PracticeDetails";
 import PracticeDentistDataTableSkeleton from "./components/skeletons/PracticeDentistDataTable";
+import { PracticeApprovalStatus } from "@prisma/client";
 
 export default async function PracticePage(props: { params: { id: string } }) {
   const { id } = props.params;
 
   return (
     <div>
-      <div className="min-h-[98vh] flex flex-col gap-5">
+      <div className="min-h-[103vh] flex flex-col gap-5">
         <PageTopBar
           pageHeading="Practices"
           showSearch={false}
