68d90a63240a0322b6c5f1df1a72d94127364a9e
quantum-builds <muhammadmassab.dev@gmail.com>
2025-10-27 19:11:59 +0500

    add: referral-ui

--- src/app/referral/page.tsx
+++ src/app/referral/page.tsx
@@ -15,22 +15,22 @@ export default async function ReferralPage() {
     : [];
 
   return (
-    // <div className="bg-feeGuide h-auto">
-    //   <HeroSection
-    //     title={"Referral Form"}
-    //     description="Aspire Dental is pleased to accept referrals from fellow dentists. And we offer referring dentists a series of promises to guarantee the best care for their patients."
-    //     backgroundColor={heroBackgroundColor}
-    //     buttonColor={herobuttonColor}
-    //     textColor="#423C36"
-    //     descLineHeight={45.4}
-    //     titleFontSize="text-[35px] md:text-[60px]"
-    //     heroScreenHieght="min-h-[55vh] md:min-h-[70vh]"
-    //   />
-    //   <div id="form">
-    //     <ReferralForm practices={practices} />
-    //   </div>
-    // </div>
-    <img src="https://aspire-media.s3.eu-west-2.amazonaws.com/uploads/aspire-clinic/images/aspire-logo.png" alt="Aspire Clinic Logo"/>
+    <div className="bg-feeGuide h-auto">
+      <HeroSection
+        title={"Referral Form"}
+        description="Aspire Dental is pleased to accept referrals from fellow dentists. And we offer referring dentists a series of promises to guarantee the best care for their patients."
+        backgroundColor={heroBackgroundColor}
+        buttonColor={herobuttonColor}
+        textColor="#423C36"
+        descLineHeight={45.4}
+        titleFontSize="text-[35px] md:text-[60px]"
+        heroScreenHieght="min-h-[55vh] md:min-h-[70vh]"
+      />
+      <div id="form">
+        <ReferralForm practices={practices} />
+      </div>
+    </div>
+    // <img src="https://aspire-media.s3.eu-west-2.amazonaws.com/uploads/aspire-clinic/images/aspire-logo.png" alt="Aspire Clinic Logo" />
 
   );
 }
