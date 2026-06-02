27d399f2593ae9dde44be955d6dff9f0137b2cc0
asif-vs <muhammadmassab.dev@gmail.com>
2026-06-02 19:49:19 +0500

    add-new-card-for-referral-form-details

--- src/app/(dashboards)/dentist/(protected)/referral-history/[id]/components/PatientReferralDetials.tsx
+++ src/app/(dashboards)/dentist/(protected)/referral-history/[id]/components/PatientReferralDetials.tsx
@@ -2,8 +2,11 @@
 
 import CustomButton from "@/app/(dashboards)/components/custom-components/CustomButton";
 import ReferralFormDetailModal from "@/app/(dashboards)/components/ReferralFormDetailModal";
+import PdfModal from "@/app/(dashboards)/components/ViewPdfModal";
+import { UploadPDFIcon } from "@/assets";
 import { usePathname } from "next/navigation";
-
+import Image from "next/image";
+import { ReadOnlyCheckbox } from "@/components/ReadOnlyCheckBox";
 interface PatientReferralDetailsProps {
   id: string;
   showModel: boolean;
@@ -49,11 +52,6 @@ export default function PatientReferralDetails({
         <p className="font-medium text-dashboardTextBlack text-2xl">
           Patient & Referral Dentist Details
         </p>
-        <CustomButton
-          text="See Referral Form Details"
-          style="secondary"
-          href={modalUrl}
-        />
       </div>
       <div className="grid grid-cols-2 gap-6">
         <div className="bg-gray p-6 space-y-5  rounded-2xl">
@@ -88,10 +86,56 @@ export default function PatientReferralDetails({
             </div>
           </div>
         )}
+        <div className="bg-gray p-6 1xl50:space-y-5 space-y-0 rounded-2xl">
+          <div className="flex justify-between items-center">
+            <p className="text-green font-medium text-2xl max-1xl50:mb-3">
+              Referral Form Details
+            </p>
+            {referralFormDetails.medicalHistoryPDF && (
+              <PdfModal
+                pdfUrl={referralFormDetails.medicalHistoryPDF}
+                trigger={
+                  <div className="flex items-center gap-3 cursor-pointer">
+                    <Image src={UploadPDFIcon} alt="PDF Icon" />
+                    <p className="underline text-green">See Document</p>
+                  </div>
+                }
+              />
+            )}
+          </div>
+          <div className="flex items-start text-lg flex-col 1xl50:flex-row 1xl50:items-start">
+            <div className="flex-1 space-y-1">
+              <p className="font-medium text-dashboardTextBlack">
+                Referral Details
+              </p>
+              <p>{referralFormDetails.referralDeatils}</p>
+            </div>
+            <div className="flex-1 space-y-1 max-1xl50:mt-3">
+              <p className="font-medium text-dashboardTextBlack">Description</p>
+              <p>
+                {referralFormDetails.treatmentDetails ? (
+                  referralFormDetails.treatmentDetails
+                ) : (
+                  <span className="italic">NO Description Added</span>
+                )}
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-between items-center text-lg max-1xl50:pt-3">
+            <div className="space-y-1">
+              <p className="font-medium text-dashboardTextBlack">
+                Would referral dentist like to attend the treatment appointment
+                with the patient and shadow the dentist?
+              </p>
+              {referralFormDetails.attendTreatment === "yes" ? (
+                <ReadOnlyCheckbox label="Yes" checked={true} />
+              ) : (
+                <ReadOnlyCheckbox label="No" checked={true} />
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-      {showModel && (
-        <ReferralFormDetailModal referralFormDetails={referralFormDetails} />
-      )}
     </div>
   );
 }
